7e3ed3274cc89d97d2c5d444dc7fa32cf199bda7
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-07-08 14:59:29 +0300

    подготовил чистый проект

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+import css from "./App.module.css";
+
+const App = () => {
+  return <div className={css.container}></div>;
+};
+
+export default App;
